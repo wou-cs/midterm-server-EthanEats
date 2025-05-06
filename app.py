af16201d176ef7fa3177c2a8ba050639c94c6ee4
Ethan DeVries <edevries21@mail.wou.edu>
2025-05-05 19:18:33 -0700

    All tests passing (problem 1 of midterm)

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+@app.route("/api/calcs/<num>", methods=["GET"])
+def calcs(num):
+    try:
+        num = int(num)
+        if num < 1:
+            return '{"Error": "Integer is non-positive"}', 404, {'Content-Type': 'application/json'}
+
+        h = hex(num)
+        d = num - 1
+
+        return f'{{ "hex" : "{h}", "dec" : {d}}}', 200, {'Content-Type': 'application/json'}   
+
+
+    except:
+        return '{"Error": "Integer is invalid"}', 400, {'Content-Type': 'application/json'}
+
